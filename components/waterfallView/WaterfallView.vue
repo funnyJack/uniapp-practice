fff74fd1a142f5f359b2267614a1ddf367e7f720
funnyJack <pengli.he@hkmci.com>
2025-03-07 11:03:51 +0800

    将瀑布流里面的内容封装成组件

---
+++ components/waterfallView/WaterfallView.vue
@@ -0,0 +1,87 @@
+<template>
+	<view class="demo-water" v-for="(item, index) in items" :key="index">
+		<up-lazy-load threshold="-350" border-radius="10" image="/static/logo.png" :index="index"></up-lazy-load>
+		<view class="demo-title">
+			{{item.title}}
+		</view>
+		<view class="demo-price">
+			{{item.times}}
+		</view>
+		<view class="demo-tag">
+			<view class="demo-tag-owner">
+				{{item.tag[0]}}
+			</view>
+			<view class="demo-tag-text">
+				{{item.tag[1]}}
+			</view>
+		</view>
+		<view class="isDot" v-if="item.isDot">
+			{{item.isDot}}
+		</view>
+	</view>
+</template>
+
+<script setup>
+	defineProps(['items'])
+</script>
+
+<style lang="scss" scoped>
+	.demo-water {
+		position: relative;
+		margin: 10rpx 10rpx 10rpx 0;
+		background-color: #fff;
+		border-radius: 16rpx;
+		padding: 16rpx
+	}
+
+	.demo-title {
+		font-size: 32rpx;
+		color: #303133;
+		margin-top: 10rpx;
+	}
+
+	.demo-price {
+		font-size: 24rpx;
+		color: #777;
+		margin-top: 10rpx;
+	}
+
+	.demo-tag {
+		display: flex;
+		margin-top: 10rpx;
+
+		.demo-tag-owner {
+			border: 1px solid rgb(252, 163, 169);
+			color: #ffaa00;
+			font-size: 20rpx;
+			display: flex;
+			align-items: center;
+			padding: 4rpx 14rpx;
+			border-radius: 50rpx;
+		}
+
+		.demo-tag-text {
+			border: 1px solid #00aaff;
+			color: #00aaff;
+			margin-left: 20rpx;
+			border-radius: 50rpx;
+			padding: 4rpx 14rpx;
+			font-size: 20rpx;
+			display: flex;
+			align-items: center;
+		}
+	}
+
+	.isDot {
+		position: absolute;
+		top: 20rpx;
+		right: 20rpx;
+		font-size: 24rpx;
+		color: #fff;
+		line-height: 32rpx;
+		background-color: #ff0000;
+		text-align: center;
+		boarder-radius: 10rpx;
+		padding: 4rpx 10rpx;
+	}
+</style>
--- pages/index/index.vue
+++ pages/index/index.vue
@@ -7,50 +7,10 @@
 		<view class="list">
 			<up-waterfall v-model="flowList" ref="uWaterfallRef">
 				<template v-slot:left="{leftList}">
-					<view class="demo-water" v-for="(item, index) in leftList" :key="index">
-						<up-lazy-load threshold="-350" border-radius="10" image="/static/logo.png"
-							:index="index"></up-lazy-load>
-						<view class="demo-title">
-							{{item.title}}
-						</view>
-						<view class="demo-price">
-							{{item.times}}
-						</view>
-						<view class="demo-tag">
-							<view class="demo-tag-owner">
-								{{item.tag[0]}}
-							</view>
-							<view class="demo-tag-text">
-								{{item.tag[1]}}
-							</view>
-						</view>
-						<view class="isDot" v-if="item.isDot">
-							{{item.isDot}}
-						</view>
-					</view>
+					<WaterfallView :items="leftList" />
 				</template>
 				<template v-slot:right="{rightList}">
-					<view class="demo-water" v-for="(item, index) in rightList" :key="index">
-						<up-lazy-load threshold="-350" border-radius="10" image="/static/logo.png"
-							:index="index"></up-lazy-load>
-						<view class="demo-title">
-							{{item.title}}
-						</view>
-						<view class="demo-price">
-							{{item.times}}
-						</view>
-						<view class="demo-tag">
-							<view class="demo-tag-owner">
-								{{item.tag[0]}}
-							</view>
-							<view class="demo-tag-text">
-								{{item.tag[1]}}
-							</view>
-						</view>
-						<view class="isDot" v-if="item.isDot">
-							{{item.isDot}}
-						</view>
-					</view>
+						<WaterfallView :items="rightList" />
 				</template>
 			</up-waterfall>
 		</view>
@@ -73,7 +33,9 @@
 		onReachBottom,
 		onPageScroll
 	} from '@dcloudio/uni-app'
-
+	
+	import WaterfallView from '@/components/waterfallView/WaterfallView.vue'
+	
 	const keyword = ref('')
 	const banners = ref([])
 	const flowList = ref([])
@@ -101,10 +63,13 @@
 			showTopButton.value = false
 		}
 	})
-	
-	
 </script>
 
+<style>
+	page{
+		background-color: rgb(240, 240, 240);
+	}
+</style>
 <style lang="scss">
 	.content {
 		padding: 20rpx 20rpx;
@@ -112,66 +77,8 @@
 
 	.list {
 		margin: 30rpx 0;
-
-		.demo-water {
-			position: relative;
-			margin: 10rpx 10rpx 10rpx 0;
-			background-color: #fff;
-			border-radius: 16rpx;
-			padding: 16rpx
 		}
 
-		.demo-title {
-			font-size: 32rpx;
-			color: #303133;
-			margin-top: 10rpx;
-		}
-
-		.demo-price {
-			font-size: 24rpx;
-			color: #777;
-			margin-top: 10rpx;
-		}
-
-		.demo-tag {
-			display: flex;
-			margin-top: 10rpx;
-
-			.demo-tag-owner {
-				border: 1px solid rgb(252, 163, 169);
-				color: #ffaa00;
-				font-size: 20rpx;
-				display: flex;
-				align-items: center;
-				padding: 4rpx 14rpx;
-				border-radius: 50rpx;
-			}
-
-			.demo-tag-text {
-				border: 1px solid #00aaff;
-				color: #00aaff;
-				margin-left: 20rpx;
-				border-radius: 50rpx;
-				padding: 4rpx 14rpx;
-				font-size: 20rpx;
-				display: flex;
-				align-items: center;
-			}
-		}
-
-		.isDot {
-			position: absolute;
-			top: 20rpx;
-			right: 20rpx;
-			font-size: 24rpx;
-			color: #fff;
-			line-height: 32rpx;
-			background-color: #ff0000;
-			text-align: center;
-			boarder-radius: 10rpx;
-			padding: 4rpx 10rpx;
-		}
-	}
 
 	.topClass {
 		position: fixed;
